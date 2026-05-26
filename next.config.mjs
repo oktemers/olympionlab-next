1103a30d7225bd92b78b965062b7f7d5b40fc18d
Oktemers <86113828+oktemers@users.noreply.github.com>
2026-05-26 03:18:40 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,19 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  poweredByHeader: false,
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
-          { key: "X-Content-Type-Options", value: "nosniff" },
-          { key: "X-Frame-Options", value: "DENY" },
-          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-          { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=(), payment=(), usb=(), browsing-topics=()" }
-        ]
-      }
-    ];
-  }
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/",
+          destination: "/index.html",
+        },
+        {
+          source: "/login.html",
+          destination: "/login",
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;

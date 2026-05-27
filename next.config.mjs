c0e8addfd6f1c9e73dca1d5aea80bf2fa1c8ee39
Oktemers <86113828+oktemers@users.noreply.github.com>
2026-05-27 22:43:03 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return {
-      beforeFiles: [
-        { source: "/", destination: "/index.html" },
-        { source: "/settings.html", destination: "/settings" },
-
-        { source: "/videos", destination: "/videos.html" },
-        { source: "/branches", destination: "/branches.html" },
-        { source: "/labs", destination: "/labs.html" },
-        { source: "/pricing", destination: "/pricing.html" },
-        { source: "/faq", destination: "/faq.html" },
-        { source: "/hakkimizda", destination: "/hakkimizda.html" },
-        { source: "/checkout", destination: "/checkout.html" },
-        { source: "/daily-problem", destination: "/daily-problem.html" },
-        { source: "/mentorship-application", destination: "/mentorship-application.html" },
-
-        { source: "/login.html", destination: "/login" },
-        { source: "/dashboard.html", destination: "/dashboard" },
-      ],
-    };
+  async redirects() {
+    return [
+      {
+        source: "/daily-problem.html",
+        destination: "/daily-problem",
+        permanent: true,
+      },
+    ];
   },
 };
 

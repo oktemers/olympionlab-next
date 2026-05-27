4bf68b3e7ed279804b64354d830fc252abc64f2e
Oktemers <86113828+oktemers@users.noreply.github.com>
2026-05-27 22:43:21 +0300

    Delete app/daily-problem/route.ts

--- app/daily-problem/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse, type NextRequest } from "next/server";
-
-export function GET(request: NextRequest) {
-  return NextResponse.redirect(new URL("/daily-problem", request.url), {
-    status: 308,
-  });
-}
